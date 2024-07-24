c39874999ec87cce65e3a0b3ccdc051a46813fb6
paula1araujo <paula1_araujo@hotmail.com>
2024-07-24 14:32:40 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,18 +21,6 @@ resource "azurerm_subnet" "subnet" {
   address_prefixes     = ["10.0.1.0/24"]
 }
 
-resource "azurerm_network_interface" "nic" {
-  name                = "wordpress-nic"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.subnet.id
-    private_ip_address_allocation = "Dynamic"
-  }
-}
-
 resource "azurerm_public_ip" "public_ip" {
   name                = "wordpress-public-ip"
   location            = azurerm_resource_group.rg.location
