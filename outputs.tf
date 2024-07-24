def45744352aa3688870dd572fdc401e34e66655
paula1araujo <paula1_araujo@hotmail.com>
2024-07-24 17:06:10 -0300

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,10 @@
-output "public_ip" {
-  value = azurerm_public_ip.public_ip.ip_address
+
+output "public_ip_address" {
+  value       = azurerm_public_ip.public_ip.ip_address
+  description = "The public IP address of the VM."
+}
+
+output "vm_id" {
+  value       = azurerm_virtual_machine.vm.id
+  description = "The ID of the virtual machine."
 }
