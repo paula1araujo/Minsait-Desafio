9d59f4b9d758fecf1dd5129877f16928b8f7f615
paula1araujo <paula1_araujo@hotmail.com>
2024-07-24 15:14:56 -0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,19 +1,23 @@
 variable "resource_group_name" {
-  description = "Name of the resource group"
+  description = "The name of the resource group."
+  type        = string
   default     = "wordpress-rg"
 }
 
 variable "location" {
-  description = "Azure location for resources"
-  default     = "East US"
+  description = "The Azure region to deploy resources."
+  type        = string
+  default     = "Canada Central"
 }
 
 variable "admin_username" {
-  description = "Admin username for the VM"
-  default     = "azureuser"
+  description = "The admin username for the VM."
+  type        = string
+  default     = "adminuser"
 }
 
 variable "admin_password" {
-  description = "Admin password for the VM"
-  default     = "P@ssw0rd1234"
+  description = "The admin password for the VM."
+  type        = string
+  default     = "adminpassword"
 }
